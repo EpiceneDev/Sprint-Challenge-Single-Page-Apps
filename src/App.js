e7b757623f3610d1bf8bae45d2608fc2fb25c886
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-23 08:37:56 -0700

    all cards made w semanitic example and displayed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterCard from './components/CharacterCard.js';
+import EpisodeCard from './components/EpisodeCard';
+import LocationCard from './components/LocationCard';
 
 
 export default function App() {
@@ -10,6 +12,8 @@ export default function App() {
       <Header />
       {/* <TabNav /> */}
       <CharacterCard />
+      <LocationCard />
+      <EpisodeCard />
     </main>
   );
 }

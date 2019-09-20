f76c4df1e561dedd0dc172a868477246734ffa6f
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-09-20 09:00:21 -0700

    styled-components added to searchform

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import { setState } from "expect/build/jestMatchersObject";
+import styled from 'styled-components';
+
+
 
 export default function SearchForm({ onSearch }) {
-  // STRETCH TODO: Add stateful logic for query/form data
+
   const [name, newName] = useState("")
 
   const handleInputChange = () => {
     setState(newName);
+  };
+
+  onSearch = () => {
+    
   }
+
+  const Input = styled.input`
+  padding 0.5em;
+  margin: 0.5 em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  boder-radius: 3px;
+  
+`
+
   return (
     <section className="search-form">
       <form onSubmit={() => onSearch(name)}>
-        <input
+        <Input
           onChange={handleInputChange}
-          placeholder="name"
+          placeholder="Character Name"
           value={name}
           name="name"
         />
